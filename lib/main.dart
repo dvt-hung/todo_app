19d2ec40602f3a53e4bb21b5ec4f7021e17504f9
Tan Hung <diepvotanhung@gmail.com>
2022-08-22 13:56:47 +0700

    set up login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp/pages/login/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,12 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("sdasda"),
-        ),
-        body: Container(),
-      ),
+      home: LoginPage(),
     );
   }
 }
